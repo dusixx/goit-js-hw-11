bac1dfb21ae24dcca7569fba842607431872f612
dusixx <113414935+dusixx@users.noreply.github.com>
2023-02-21 04:25:26 +0200

    adds elementary styles

--- src/index.js
+++ src/index.js
@@ -1,4 +1,16 @@
 import './css/styles.css';
+// import './sass/main.scss';
 // import axios from 'axios';
 // import { Notify } from 'notiflix';
 // import simpleLightbox from 'simplelightbox';
+
+const searchInputRef = document.querySelector('.search-form__input');
+const clearInputBtnRef = document.querySelector('.search-form__btn-clear');
+
+clearInputBtnRef.addEventListener('click', e => {
+  searchInputRef.value = '';
+});
+
+searchInputRef.addEventListener('focus', e => {
+  //   e.currentTarget.select();
+});
