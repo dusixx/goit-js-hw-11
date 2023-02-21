fcb66cb55c5c746db568ca655286d3de6f24df73
dusixx <113414935+dusixx@users.noreply.github.com>
2023-02-21 16:43:05 +0200

    adds styles

--- src/index.js
+++ src/index.js
@@ -1,16 +1,23 @@
-import './css/styles.css';
+import './sass/index.scss';
 // import './sass/main.scss';
 // import axios from 'axios';
 // import { Notify } from 'notiflix';
 // import simpleLightbox from 'simplelightbox';
 
+const searchFormRef = document.querySelector('.search-form');
 const searchInputRef = document.querySelector('.search-form__input');
 const clearInputBtnRef = document.querySelector('.search-form__btn-clear');
 
 clearInputBtnRef.addEventListener('click', e => {
   searchInputRef.value = '';
+  // чтобы не слетал outline
+  searchInputRef.focus();
 });
 
 searchInputRef.addEventListener('focus', e => {
   //   e.currentTarget.select();
+});
+
+searchFormRef.addEventListener('submit', e => {
+  e.preventDefault();
 });
