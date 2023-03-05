2029fdd98e5e20fb86fd8547e421b9f06b0186e1
dusixx <113414935+dusixx@users.noreply.github.com>
2023-03-05 01:42:44 +0000

    Deploying to gh-pages from @ dusixx/goit-js-hw-11@0f7d07ef24a4bed310bc926c442ea6221e813874 🚀

--- index.ad7cfd6f.js
+++
@@ -1,2 +0,0 @@
-!function(){var e=document.querySelector(".search-form"),t=document.querySelector(".search-form__input");document.querySelector(".search-form__btn-clear").addEventListener("click",(function(e){t.value="",t.focus()})),t.addEventListener("focus",(function(e){})),e.addEventListener("submit",(function(e){e.preventDefault()}))}();
-//# sourceMappingURL=index.ad7cfd6f.js.map
