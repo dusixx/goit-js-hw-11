a347bd8a744347ab8392c18a0756fa2079dcf1d1
dusixx <113414935+dusixx@users.noreply.github.com>
2023-03-21 11:45:06 +0200

    some fixes

--- src/js/media-filter.js
+++ src/js/media-filter.js
@@ -4,18 +4,21 @@ import queryParams from './rest-data';
 
 const { mediaFilter, toggleMediaFilter } = refs;
 
-const BUTTON_CLASS = `${mediaFilter.className}__btn`;
-const MEDIA_FILTER_HIDDEN_CLASS = 'media-filter--hidden';
+const MEDIA_FILTER_CLASS = mediaFilter.className;
+const MEDIA_FILTER_HIDDEN_CLASS = `${MEDIA_FILTER_CLASS}--hidden`;
+const BUTTON_CLASS = `${MEDIA_FILTER_CLASS}__btn`;
 
-makeMediaFilterPanel(mediaFilter);
+toggleMediaFilter.addEventListener('click', () =>
+  mediaFilter.classList.toggle(MEDIA_FILTER_HIDDEN_CLASS)
+);
 
-toggleMediaFilter.addEventListener('click', handleToggleMediaFilterClick);
+//
+// Funcs
+//
 
-function handleToggleMediaFilterClick(e) {
-  mediaFilter.classList.toggle(MEDIA_FILTER_HIDDEN_CLASS);
-}
+makeMediaFilterPanel(mediaFilter, queryParams);
 
-function makeMediaFilterPanel(mediaFilter) {
+function makeMediaFilterPanel(mediaFilter, queryParams) {
   const markup = Object.entries(queryParams)
     .map(([name, { caption, value, alias }]) => {
       // bool -> checkbox
